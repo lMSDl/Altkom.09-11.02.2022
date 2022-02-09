f6057392b3b6b1a930be29bc24c07bb20632abd9
Paweł Łęczycki <pawel@msd.info.pl>
2022-02-09 14:00:54 +0100

    Builder

---
+++ ConsoleApp/DesignPatterns/Creational/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.DesignPatterns.Creational
+{
+    public class Client
+    {
+        public static void Execute()
+        {
+            //var vehicle = new Vehicle(4, 5, 4, 500, 100);
+            var vehicleBuilder = new VehicleBuilder();
+            vehicleBuilder.SetWeels(4);
+            vehicleBuilder.SetSeats(5);
+            vehicleBuilder.SetDoors(4);
+            vehicleBuilder.SetTrunkCapacity(500);
+            vehicleBuilder.SetEnginePower(100);
+
+            //var vehicle = vehicleBuilder.Build();
+
+           
+
+            var vehicle = new VehicleBuilder()
+            .SetWeels(4)
+            .SetSeats(5)
+            .SetDoors(4)
+            .SetTrunkCapacity(500)
+            .SetEnginePower(100)
+            .Build();
+
+
+            Console.WriteLine(vehicle);
+        }
+    }
+}
---
+++ ConsoleApp/DesignPatterns/Creational/VehicleBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.DesignPatterns.Creational
+{
+    public class VehicleBuilder
+    {
+        private Vehicle _vehicle = new Vehicle();
+
+        public VehicleBuilder SetWeels(int wheels)
+        {
+            _vehicle.Wheels = wheels;
+            return this;
+        }
+
+        public VehicleBuilder SetDoors(int doors)
+        {
+            _vehicle.Doors = doors;
+            return this;
+        }
+
+        public VehicleBuilder SetEnginePower(int power)
+        {
+            _vehicle.EnginePower = power;
+            return this;
+        }
+
+        public VehicleBuilder SetTrunkCapacity(int capacity)
+        {
+            _vehicle.TrunkCapacity = capacity;
+            return this;
+        }
+
+        public VehicleBuilder SetSeats(int seats)
+        {
+            _vehicle.Seats = seats;
+            return this;
+        }
+
+        public Vehicle Build()
+        {
+            return _vehicle;
+        }
+    }
+}
