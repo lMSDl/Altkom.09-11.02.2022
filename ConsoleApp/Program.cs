0cf4305a243230083227fe6ed8ae7cab265816fa
Paweł Łęczycki <pawel@msd.info.pl>
2022-02-09 12:34:58 +0100

    LSP

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp.SOLID.L2;
+using System;
 
 namespace ConsoleApp
 {
@@ -6,7 +7,16 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle rectangle = new Square();
+            ShowArea(rectangle, 3, 5);
+
+        }
+
+        static void ShowArea(Rectangle rectangle, int a, int b)
+        {
+            rectangle.A = a;
+            rectangle.B = b;
+            Console.WriteLine($"Pole prostokąta o bokach {a} i {b} jest równe {rectangle.Area}");
         }
     }
 }
