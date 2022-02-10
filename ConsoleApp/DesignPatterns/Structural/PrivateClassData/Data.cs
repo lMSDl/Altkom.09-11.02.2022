7aa1fa05f23980324426084b1fc5e22a21a9e265
Paweł Łęczycki <pawel@msd.info.pl>
2022-02-10 15:05:44 +0100

    PrivateClassData

---
+++ ConsoleApp/DesignPatterns/Structural/PrivateClassData/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.DesignPatterns.Structural.PrivateClassData
+{
+    public class Data
+    {
+        private PrivateData _privateData;
+
+        public Data(int a, string b)
+        {
+            _privateData = new PrivateData(a, b);
+        }
+
+        public void SomeFunction()
+        {
+            _privateData.GetA();
+        }
+    }
+}
---
+++ ConsoleApp/DesignPatterns/Structural/PrivateClassData/PrivateData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.DesignPatterns.Structural.PrivateClassData
+{
+    class PrivateData
+    {
+        private int _a;
+        private string _b;
+
+        public PrivateData(int a, string b)
+        {
+            _a = a;
+            _b = b;
+        }
+
+        public int GetA()
+        {
+            return _a;
+        }
+
+        public string GetB()
+        {
+            return _b;
+        }
+    }
+}
